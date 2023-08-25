87fd08d303701a4dc4071ccde1f9d828f58b3615
Trishita sINGH <trishitasingh@eliza.cooper>
2023-08-25 09:12:12 -0400

    try catch update

--- app.py
+++ app.py
@@ -18,26 +18,29 @@ csv_writer = csv.writer(csv_file)
 csv_writer.writerow(['Subject', 'Start Time', 'End Time', 'Description'])
 
 # Read the iCalendar file
-cal = Calendar.from_ical(ics_file.read())
-for event in cal.walk('VEVENT'):
-    print(event)
-    subject = event['SUMMARY']
-    start_time = event['DTSTART'].dt
-    end_time = ""
-    if 'DTEND' in event:
-        end_time = event['DTEND'].dt
-    description = ''
-    if 'DESCRIPTION' in event:
-        description = event['DESCRIPTION']
-    csv_writer.writerow([subject, start_time, end_time, description])
+try:
+    cal = Calendar.from_ical(ics_file.read())
+    for event in cal.walk('VEVENT'):
+        print(event)
+        subject = event['SUMMARY']
+        start_time = event['DTSTART'].dt
+        end_time = ""
+        if 'DTEND' in event:
+            end_time = event['DTEND'].dt
+        description = ''
+        if 'DESCRIPTION' in event:
+            description = event['DESCRIPTION']
+        csv_writer.writerow([subject, start_time, end_time, description])
 
-# Close the CSV file
-csv_file.close()
+    # Close the CSV file
+    csv_file.close()
 
-# Display a link to download the CSV file
-st.markdown('### **Download CSV File**')
+    # Display a link to download the CSV file
+    st.markdown('### **Download CSV File**')
 
-filename = "calendar.csv"
-with open(filename, 'rb') as f:
-    bytes = f.read()
-    st.download_button(label='Download file', data=bytes, file_name=filename, mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    filename = "calendar.csv"
+    with open(filename, 'rb') as f:
+        bytes = f.read()
+        st.download_button(label='Download file', data=bytes, file_name=filename, mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+except:
+    st.write("Upload a file")
